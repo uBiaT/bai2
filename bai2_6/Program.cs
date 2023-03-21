1791b001b424d851410bed447133f185636e76ca
Tai Anh Truong <truonganhtai.10a6@gmail.com>
2023-03-21 17:15:47 +0700

    dang lam bai 6

---
+++ bai2_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap ngay thang nam: ");
+            var theDay = DateTime.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("<--------------------------------------------------->");
+
+            //Console.WriteLine($"ho ten hoc sinh: {hoTen}");
+            //Console.WriteLine($"lop: {lop}");
+            //Console.WriteLine($"diem trung binh ca nam: {(diemHKI + diemHKII * 2) / 3}");
+
+
+            Console.WriteLine("<--------------------------------------------------->");
+
+            Console.WriteLine("nhap phim bat ki de ket thuc");
+            Console.ReadKey();
+        }
+    }
+}
