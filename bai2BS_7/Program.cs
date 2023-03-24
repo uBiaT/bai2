117c0ab3e8ffbdec9e01140b6ca285ca090ce5fe
Tai Truong <truonganhtai.10a6@gmail.com>
2023-03-25 00:06:43 +0700

    xong bai 2_8 bo sung

---
+++ bai2BS_7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2BS_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<----------------------INPUT---------------------->");
+                Console.Write("nhap ho ten: ");
+                string hoTen = Console.ReadLine();
+            Console.WriteLine();
+
+            int soLuong = hoTen.Length;
+
+            hoTen = hoTen.Trim();
+
+            string ho = hoTen.Substring(0, hoTen.IndexOf(" "));
+            string ten = hoTen.Substring(hoTen.LastIndexOf(" "), hoTen.Length - hoTen.LastIndexOf(" "));
+            string dem = hoTen.Substring(hoTen.IndexOf(" "), hoTen.LastIndexOf(" ") - hoTen.IndexOf(" "));
+
+            Console.WriteLine("<----------------------OUTPUT--------------------->");
+                Console.WriteLine($"ban vua nhap {soLuong} ky tu");
+                Console.WriteLine($"ho: {ho.Trim()}");
+                Console.WriteLine($"dem: {dem.Trim()}");
+                Console.WriteLine($"ten: {ten.Trim()}");
+            Console.WriteLine();
+
+            Console.WriteLine("<----------------------END------------------------>");
+            Console.WriteLine("nhap phim bat ki de ket thuc");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bai2BS_8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2BS_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<----------------------INPUT---------------------->");
+                Console.Write("ten hang hoa: ");
+                string tenHang = Console.ReadLine();
+
+                Console.Write("so luong: ");
+                int soLuong = int.Parse(Console.ReadLine());
+
+                Console.Write("don gia: ");
+                double donGia = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            double tongTien = donGia * soLuong * 1.08;
+
+            Console.WriteLine("<----------------------OUTPUT--------------------->");
+                Console.WriteLine($"so tien ban phai tra la cho {soLuong} {tenHang} la {tongTien}");
+            Console.WriteLine();
+
+            Console.WriteLine("<----------------------END------------------------>");
+            Console.WriteLine("nhap phim bat ki de ket thuc");
+            Console.ReadKey();
+        }
+    }
+}
